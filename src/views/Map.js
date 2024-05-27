dfaee721c0dd3f5f9a30553d0b8093340336bea4
Olga Tryk <oltryk@gmail.com>
2024-05-27 18:55:30 +0200

    Map centers at detected location on page refresh

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,34 +1,30 @@
-import React, {useState} from "react";
-import {MapContainer, TileLayer, useMapEvents} from "react-leaflet"
+import React, {useEffect} from "react";
+import {MapContainer, TileLayer, useMap} from "react-leaflet"
 import {useNavigate} from "react-router-dom";
 import IconButton from '@mui/material/IconButton';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-// click on the map to recenter to current location
-function Locate() {
-    const [position, setPosition] = useState(null)
-    const map = useMapEvents({
-        click() {
-            map.locate()
-        },
-        locationfound(e) {
-            setPosition(e.latlng)
-            map.flyTo(e.latlng, map.getZoom())
-        },
-    })
+const Recenter = () => {
+    const map = useMap();
+    useEffect(() => {
+        map.locate().on("locationfound", function (e) {
+            map.setView(e.latlng);
+        })
+    });
+    return null;
 }
 
 function Map() {
     let navigate = useNavigate();
     return (
-        <MapContainer center={[51.10783984131171, 17.06110885722262]} zoom={20} scrollWheelZoom={true}>
+        <MapContainer center={[51.10783984131171,17.06110885722262]} zoom={20} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Locate/>
-            <IconButton className="showUserInfo" size="large" onClick={() => navigate("/userInfo")}>
-                <AccountCircleIcon className="userIcon" />
+            <Recenter />
+            <IconButton className="showUserInfo" fontSize="large" onClick={() => navigate("/userInfo")}>
+                <AccountCircleIcon className="userIcon" fontSize="large"/>
             </IconButton>
         </MapContainer>
     );
